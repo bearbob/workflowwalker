49826a850de5df1c8d039e5acdd7127d63046f9d
B. Gross <mynyxi@gmail.com>
2017-04-06 16:34:43 +0200

    Added Apache 2 license

--- src/instances/CuneiformWalker.java
+++ src/instances/CuneiformWalker.java
@@ -5,6 +5,24 @@ import sampler.*;
 
 import java.util.ArrayList;
 
+/*
+		% WorkflowWalker: A Workflow Parameter Optimizer
+		%
+		% Copyright 2017 Björn Groß and Raik Otto
+		%
+		% Licensed under the Apache License, Version 2.0 (the "License");
+		% you may not use this file except in compliance with the License.
+		% You may obtain a copy of the License at
+		%
+		%    http://www.apache.org/licenses/LICENSE-2.0
+		%
+		% Unless required by applicable law or agreed to in writing, software
+		% distributed under the License is distributed on an "AS IS" BASIS,
+		% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		% See the License for the specific language governing permissions and
+		% limitations under the License.
+*/
+
 // TODO: This class is not ready to use but a collection of ideas how one might implement the walker with Cuneiform
 
 public class CuneiformWalker extends Walker {
--- src/instances/PloidyRnaWalker.java
+++ src/instances/PloidyRnaWalker.java
@@ -9,6 +9,24 @@ import sampler.IntegerParameter;
 import sampler.Parameter;
 import sampler.Step;
 
+/*
+		% WorkflowWalker: A Workflow Parameter Optimizer
+		%
+		% Copyright 2017 Björn Groß and Raik Otto
+		%
+		% Licensed under the Apache License, Version 2.0 (the "License");
+		% you may not use this file except in compliance with the License.
+		% You may obtain a copy of the License at
+		%
+		%    http://www.apache.org/licenses/LICENSE-2.0
+		%
+		% Unless required by applicable law or agreed to in writing, software
+		% distributed under the License is distributed on an "AS IS" BASIS,
+		% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+		% See the License for the specific language governing permissions and
+		% limitations under the License.
+*/
+
 public class PloidyRnaWalker extends RnaWalker {
 	
 	public PloidyRnaWalker(LogDB logdb, String runName, String basedir, String inputFile, int threads, TargetFunction target) {
